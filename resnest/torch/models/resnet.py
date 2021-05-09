5a1140f555352b9f7817a6ca5ec5a652ecb82d61
Mohit Kumar <64537818+mohitktanwr@users.noreply.github.com>
2021-05-09 20:13:48 +0530

    Update resnet.py

--- resnest/torch/models/resnet.py
+++ resnest/torch/models/resnet.py
@@ -192,7 +192,7 @@ class ResNet(nn.Module):
         conv_kwargs = {'average_mode': rectify_avg} if rectified_conv else {}
         if deep_stem:
             self.conv1 = nn.Sequential(
-                conv_layer(3, stem_width, kernel_size=3, stride=2, padding=1, bias=False, **conv_kwargs),
+                conv_layer(3, stem_width, kernel_size=3, stride=1, padding=1, bias=False, **conv_kwargs), #stride = 2
                 norm_layer(stem_width),
                 nn.ReLU(inplace=True),
                 conv_layer(stem_width, stem_width, kernel_size=3, stride=1, padding=1, bias=False, **conv_kwargs),
@@ -201,7 +201,7 @@ class ResNet(nn.Module):
                 conv_layer(stem_width, stem_width*2, kernel_size=3, stride=1, padding=1, bias=False, **conv_kwargs),
             )
         else:
-            self.conv1 = conv_layer(3, 64, kernel_size=7, stride=2, padding=3,
+            self.conv1 = conv_layer(3, 64, kernel_size=7, stride=1, padding=3, #stride = 2
                                    bias=False, **conv_kwargs)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
