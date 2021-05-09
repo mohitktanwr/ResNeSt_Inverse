3347fe727a0f377d0d1312070e98b14c09e49747
Mohit Kumar <64537818+mohitktanwr@users.noreply.github.com>
2021-05-09 20:36:11 +0530

    Update resnet.py

--- resnest/torch/models/resnet.py
+++ resnest/torch/models/resnet.py
@@ -93,7 +93,7 @@ class Bottleneck(nn.Module):
 
         self.conv3 = nn.Conv2d(
             group_width, planes , kernel_size=1, bias=False) #planes*4
-        self.bn3 = norm_layer(planes*4) #planes*4
+        self.bn3 = norm_layer(planes) #planes*4
 
         if last_gamma:
             from torch.nn.init import zeros_
